de3f003d5bd14abcd04eae31ff91720ed08a0c4d
Yilun Xu <yilun.xu@oath.com>
2018-11-01 13:51:46 -0700

    iOS working

--- FlurryWatch_Swift/AppDelegate.swift
+++ FlurryWatch_Swift/AppDelegate.swift
@@ -15,7 +15,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        Flurry.startSession("6QSG5T6M8FJTJ9F3NKNW");
+        
         return true
     }
 
